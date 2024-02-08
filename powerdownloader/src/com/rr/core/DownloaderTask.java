f34a3041e269db95104c899a5c7bae7d79d5153f
thinkerr24 <thinkerr24@163.com>
2024-02-08 21:07:44 +0800

    chunk task

---
+++ powerdownloader/src/com/rr/core/DownloaderTask.java
@@ -0,0 +1,69 @@
+package com.rr.core;
+
+import com.rr.constant.Constant;
+import com.rr.utils.HttpUtils;
+import com.rr.utils.LogUtils;
+
+import java.io.BufferedInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.net.HttpURLConnection;
+import java.util.concurrent.Callable;
+
+/**
+ * Chunk download task
+ */
+public class DownloaderTask implements Callable<Boolean> {
+
+    private String url;
+
+    // Download start position
+    private long startPos;
+
+    // Download end position
+    private long endPos;
+
+    // Identify which part of current download
+    private int part;
+
+    public DownloaderTask(String url, long startPos, long endPos, int part) {
+        this.url = url;
+        this.startPos = startPos;
+        this.endPos = endPos;
+        this.part = part;
+    }
+
+    @Override
+    public Boolean call() throws Exception {
+        // Get filename
+        String httpFileName = HttpUtils.getHttpFileName(url);
+        // Chunk file name
+        httpFileName = httpFileName + ".temp" + part;
+        // Download path
+        httpFileName = Constant.DOWNLOAD_PATH + httpFileName;
+        // Get chunk download url
+        HttpURLConnection httpURLConnection = HttpUtils.getHttpURLConnection(url, startPos, endPos);
+        try (
+                InputStream input = httpURLConnection.getInputStream();
+                BufferedInputStream bis = new BufferedInputStream(input);
+                RandomAccessFile accessFile = new RandomAccessFile(httpFileName, "rw");
+        ) {
+
+            byte[] buffer = new byte[Constant.BYTE_SIZE];
+            int len = -1;
+            while ((len = bis.read()) != -1) {
+                accessFile.write(buffer, 0, len);
+            }
+        } catch (FileNotFoundException e) {
+            LogUtils.error("File not exist{}.", url);
+            return false;
+        } catch (Exception e) {
+            LogUtils.error("Download exception.");
+            return false;
+        } finally {
+            httpURLConnection.disconnect();
+        }
+        return true;
+    }
+}
--- powerdownloader/src/com/rr/utils/HttpUtils.java
+++ powerdownloader/src/com/rr/utils/HttpUtils.java
@@ -8,8 +8,29 @@ import java.net.URL;
  * http util class
  */
 public class HttpUtils {
+
     /**
+     * Chunk download
      *
+     * @param url      file url
+     * @param startPos
+     * @param endPos
+     * @return
+     * @throws IOException
+     */
+    public static HttpURLConnection getHttpURLConnection(String url, long startPos, long endPos) throws IOException {
+        HttpURLConnection httpURLConnection = getHttpURLConnection(url);
+        LogUtils.info("Download position: from {} to {}", startPos, endPos);
+        if (endPos != 0) {
+            // bytes=100-200
+            httpURLConnection.setRequestProperty("RANGE", "bytes=" + startPos + "-" + endPos);
+        } else {
+            httpURLConnection.setRequestProperty("RANGE", "bytes=" + startPos + "-");
+        }
+        return httpURLConnection;
+    }
+
+    /**
      * @param url file url
      * @return
      */
@@ -22,12 +43,13 @@ public class HttpUtils {
     }
 
     /**
-     *  get download file name
+     * get download file name
+     *
      * @param url
      * @return
      */
     public static String getHttpFileName(String url) {
-       int index = url.lastIndexOf("/");
-       return url.substring(index + 1);
+        int index = url.lastIndexOf("/");
+        return url.substring(index + 1);
     }
 }
